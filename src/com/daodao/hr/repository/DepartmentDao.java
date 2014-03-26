30b9a53a4e33ce46e29c0ec871bbac09685b9b86
gao chao <daodao_daodao@hotmail.com>
2014-03-26 17:22:52 +0800

    Revert "Revert "0326""
    This reverts commit fab44391fcc46fcb1084423c8fb6edcc4c6e8fea.

---
+++ src/com/daodao/hr/repository/DepartmentDao.java
@@ -0,0 +1,8 @@
+package com.daodao.hr.repository;
+
+import com.daodao.common.repository.BaseDao;
+import com.daodao.hr.entity.Dept;
+
+public interface DepartmentDao  extends BaseDao<Dept>{
+
+}
--- src/com/daodao/hr/web/DepartmentController.java
+++ src/com/daodao/hr/web/DepartmentController.java
@@ -1,19 +1,40 @@
 package com.daodao.hr.web;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.daodao.hr.entity.Dept;
+import com.daodao.hr.service.DepartmentService;
+
 @Controller
 @RequestMapping(value = "/daodao/hr/dept")
 public class DepartmentController {
+	
+	@Autowired
+	DepartmentService deptService;
 
 	@RequestMapping(value = "list")
-	public String test(Model model) {
+	public String deptList(Model model) {
 		
+		String hql = "from Dept where deleted = false";
 		
+		Page<Dept> depts =  this.deptService.getHqlPageInfo(hql, 10, 1, Arrays.asList());
+		
+		model.addAttribute("depts", depts);
 		
 		return "/hr/departmentList";
+	}
+	
+	@RequestMapping(value = "add")
+	public String addDept(){
+		
+		return "/hr/addDept";
+		
 	}
 
 }
